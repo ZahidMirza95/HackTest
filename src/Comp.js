d6cae6e8d76f2e7768c3c081c0dcd0adcb33a772
Zamzam95-Coder <zam19@rogers.com>
2021-01-13 22:20:22 -0500

    pushed files

---
+++ src/Comp.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+/* Alternative implementation */
+/*export class Comp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        }
+    }
+
+    render () {
+        return (
+            <div className = "Comp">
+                <h1> {this.props.name} </h1>
+            </div>
+        );
+    }
+}*/
+
+export const Comp = (props) => {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div className = "main">
+            <h1> {props.name} </h1>
+        </div>
+    );
+}
